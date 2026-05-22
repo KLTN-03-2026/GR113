bb7e1da9b638b9a5308e10769d1acaf1e07202cf
NguyenCKhanhTai <taibwin210365@gmail.com>
2026-05-22 12:21:48 +0700

    update thay doi

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using demomvc.Helpers;
+using demomvc.Models;
 
 namespace demomvc
 {
@@ -13,6 +16,52 @@ namespace demomvc
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Task.Run(() =>
+            {
+                using (var db = new QuanLyTruongHocEntities2())
+                {
+                    var ds = db.Diem
+                        .Where(d => d.DiemTB != null)
+                        .Select(d => new
+                        {
+                            d.HocSinhID,
+                            d.HocKyID,
+                            d.NamHocID
+                        })
+                        .Distinct()
+                        .ToList();
+
+                    var service = new KetQuaService();
+
+                    foreach (var item in ds)
+                    {
+                        service.UpdateKQ(
+                            item.HocSinhID,
+                            item.HocKyID,
+                            item.NamHocID
+                        );
+                    }
+
+
+                    var dsNam = db.KetQuaHocTap
+                    .Where(k => k.DTBTong != null && k.HocKyID == 2)
+                    .Select(k => new
+                    {
+                        k.HocSinhID,
+                        k.NamHocID
+                    })
+                    .Distinct()
+                    .ToList();
+
+                    foreach (var item in dsNam)
+                    {
+                        service.UpdateDiemCaNam(
+                            item.HocSinhID,
+                            item.NamHocID
+                        );
+                    }
+                }
+            });
         }
     }
 }
